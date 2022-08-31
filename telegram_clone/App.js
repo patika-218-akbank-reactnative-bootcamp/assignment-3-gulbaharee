89a08918a10043427fb4225cf9b47632fe59e5f1
Gülbahar Erol <ggulbaharerol@gmail.com>
2022-08-31 19:42:28 +0300

    project init

---
+++ telegram_clone/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      
+       
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
