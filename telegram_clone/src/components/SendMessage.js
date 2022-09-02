f75ce7416368af51d1e71840a43bea0bf630e146
Gülbahar Erol <ggulbaharerol@gmail.com>
2022-09-02 22:38:02 +0300

    setting page adadded

--- telegram_clone/src/components/SendMessage.js
+++ telegram_clone/src/components/SendMessage.js
@@ -4,6 +4,27 @@ import IconEntypo from 'react-native-vector-icons/Entypo';
 import IconSimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 
 const SendMessage = ({ id }) => {
+    // // input için tuttuğunuz state
+    // const [message, setMessage] = useState("");
+    // // context erişimi (degişken isimleri farklı olabilir)
+    // const { users, setUsers } = useContext(UserContext)
+
+    // // send message fonksiyonunun yapacağıni işlem, kurduğunuz yapıya göre değişebilir.
+    // const sendMessage = () => {
+    //     const newUsers = users.map(user => {
+    //         // {id: 1, username: "atakan", messages: []}
+    //         if (user.id === params.user.id) {
+    //             const newMessages = user.messages.push({ text: "Merhaba", date: new Date.now() })
+    //             return {
+    //                 ...user,
+    //                 messages: newMessages
+    //             }
+    //         } else {
+    //             return user
+    //         }
+    //     })
+    //     setUsers(newUsers)
+    // }
 
     return (
         <View>
@@ -15,12 +36,13 @@ const SendMessage = ({ id }) => {
             />
             <View style={styles.container}>
                 <IconEntypo name="attachment" style={{ marginTop: 20, marginLeft: 20 }} size={25} />
-                <TextInput placeholder='write a message' style={styles.messagebox}/>
-                <IconSimpleLineIcons style={{ marginTop: 15,marginRight:20}} size={25} name="microphone"/>
+                <TextInput placeholder='write a message' style={styles.messagebox} />
+                <IconSimpleLineIcons style={{ marginTop: 15, marginRight: 20 }} size={25} name="microphone" />
             </View>
         </View>
 
     )
+
 }
 export default SendMessage;
 
@@ -30,15 +52,15 @@ const styles = StyleSheet.create(
             height: 100,
             display: "flex",
             flexDirection: "row",
-            justifyContent:"space-between"
+            justifyContent: "space-between"
         },
-        messagebox:{
-            marginTop:15,
-            borderRadius:20,
-            borderWidth:1,
-            borderColor:'gray',
-            width:250,
-            height:30,
+        messagebox: {
+            marginTop: 15,
+            borderRadius: 20,
+            borderWidth: 1,
+            borderColor: 'gray',
+            width: 250,
+            height: 30,
         }
     }
 )
--- telegram_clone/src/pages/Setting.js
+++ telegram_clone/src/pages/Setting.js
@@ -1,12 +1,81 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image, Pressable } from 'react-native';
+import IconFoundation from 'react-native-vector-icons/Foundation';
+import IconIonicons from 'react-native-vector-icons/Ionicons';
 
 const Setting = () => {
     return (
-        <View>
-            <Text>Setting</Text>
+        <View style={styles.container}>
+            <View style={styles.info}>
+                <Image source={{ uri: "https://picsum.photos/200/200" }} style={styles.userImage} />
+                <Text style={styles.userName}>Gulbahar Erol</Text>
+                <Text style={styles.userInfo}>xxx-xxx-xx-xx @username</Text>
+            </View>
+            <View style={styles.settings}>
+                <Pressable style={styles.button} title='Theme'>
+                    <IconFoundation style={{ marginTop: 15, marginRight: 20, color:"green" }} size={25} name="contrast" />
+                    <Text style={styles.buttonText}>Theme</Text>
+                    <IconIonicons name="chevron-forward" style={{ marginTop: 15, marginLeft: 10 }} size={25} />
+                </Pressable>
+                <Pressable style={styles.button} title='Settings'>
+                    <IconIonicons style={{ marginTop: 15, marginRight: 20 , color:"purple"}} size={25} name="settings" />
+                    <Text style={styles.buttonText}>Settings</Text>
+                    <IconIonicons name="chevron-forward" style={{ marginTop: 15, marginLeft: 10 }} size={25} />
+                </Pressable>
+            </View>
         </View>
     )
 }
 
 export default Setting;
+
+const styles = StyleSheet.create(
+    {
+        container: {
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+        },
+        info: {
+            height: 300,
+            justifyContent: "center",
+        },
+        settings: {
+            margin: 30,
+            height: 120,
+            backgroundColor: "white",
+            display: "flex",
+            flexDirection: "column",
+        },
+        userImage: {
+            alignSelf: "center",
+            width: 120,
+            height: 120,
+            borderRadius: 60,
+        },
+        userName: {
+            textAlign: "center",
+            marginTop: 15,
+            fontSize: 30,
+        },
+        userInfo: {
+            textAlign: "center",
+            fontSize: 20,
+            marginTop: 10,
+            color: "rgb(200,200,200)",
+        },
+        button: {
+            height: 60,
+            display:"flex",
+            flexDirection:"row",
+            marginLeft:10,
+            marginRight:10,
+            justifyContent:"space-between",
+
+        },
+        buttonText: {
+            marginTop: 20,
+            width:200,
+        }
+    }
+)
